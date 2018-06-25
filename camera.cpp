a668c7d4837969f10e8d92c0d97d08e2335d6259
RubenD Prieto <rubendariopm14@gmail.com>
2018-06-25 13:44:46 -0400

    updates for block editing :)

---
+++ camera.cpp
@@ -0,0 +1,9 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+Mat getFrame(){
+	
+	
+	
+}
--- main.cpp
+++ main.cpp
@@ -5,29 +5,22 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-	// if ( argc != 2 )
-	// {
-	// 	printf("usage: DisplayImage.out <Image_Path>\n");
-    //     return -1;
-    // }
-
-    Mat image;
-    image = imread("logo.png", 1 );
-
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-	
-    namedWindow("Image", WINDOW_AUTOSIZE);
+	Mat image;
+	image = imread("logo.png", 1 );
 	Mat resized;
 	resize(image, resized, Size(400,400));
-    imshow("Image", resized);
 	
-	// resizeWindow("Image", Size(500,500));
+	// namedWindow("Image", WINDOW_AUTOSIZE);/
+	imshow("Image", resized);
+// 
+	VideoCapture vc = VideoCapture(0);
 	
-    waitKey();
+	while(waitKey(30) == -1){ //While nothing is pressed
+		namedWindow("Video", WINDOW_AUTOSIZE);
+		Mat vidbuffer;
+		
+	}
+	waitKey(0);
 	
 	return 0;
 }
