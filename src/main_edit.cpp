e7cf4876862d2d29a19d6c632938074d20647fa1
RubenD <rubendariopm14@gmail.com>
2018-09-03 21:12:09 -0400

    Still working on main_edit

--- src/main_edit.cpp
+++ src/main_edit.cpp
@@ -15,10 +15,14 @@ fs::path current_path;
 bool is_image = false;
 
 const int K_LEFT = 81, K_UP = 82, K_RIGHT = 83, K_DOWN = 84, K_ESC = 27;
+const int K_a = 97, K_s = 115, K_d = 100, K_w = 119;
+const int K_f = 102, K_g = 103, K_h = 104, K_t = 116;
+const int K_shift = 225, K_ctrl = 227;
 const int K_R = 114;
 
 Mat image;
 Object o;
+Browser *br;
 
 auto header_size = Size(600,30);
 
@@ -38,10 +42,11 @@ void update_image(){
 	double font_size = 0.5;
 	double font_width = 1;
 
-	auto count_text = (boost::format("%1%/%2%") % it_index % it_max).str();
+	auto count_text = (boost::format("%1%/%2%") % br->file_count % br->availables.size()).str();
 	putText(header, count_text, Point(2,height+4), font_face, font_size, font_color, font_width);
 	
-	auto path_size = getTextSize(current_path.string(), font_face, font_size, font_width, &baseline);
+	string path_string = br->getPath().string();
+	auto path_size = getTextSize(, font_face, font_size, font_width, &baseline);
 	putText(header, current_path.string(), Point(header_size.width/2 - path_size.width/2,height), 
 		font_face, font_size, font_color, font_width);
 
@@ -62,6 +67,12 @@ void update_image(){
 	imshow("Editing", to_show);
 }
 
+void load_image(){
+	if (br->is_valid() && !br->directory){
+		image = imread(br->getPath());
+	}else image.release();
+}
+
 int main(int argc, char** argv)
 {
 	setArgs(argc, argv);
@@ -71,9 +82,13 @@ int main(int argc, char** argv)
 	if (!fs::exists(rawPath) || !fs::is_directory(rawPath)){cout << "The path must exist and it must be a directory" << endl; return 1;}
 	
 	Browser browser(rawPath);
+	br = &browser;
 	
 	current_path = browser.getPath();
 	update_image();
+	
+	int xy_move_speed = 1;
+	int wh_move_speed = 1;
 
 	while(1){
 		auto key = waitKey(0);
@@ -81,23 +96,58 @@ int main(int argc, char** argv)
 			case K_ESC:
 				return 0;
 				break;
+			
+			// Browser inupt
 			case K_LEFT:
 				browser.out();
+				load_image();
 				break;
 			case K_RIGHT:
 				browser.in();
+				load_image();
 				break;
 			case K_UP:
 				browser.prev();
+				load_image();
 				break;
 			case K_DOWN:
 				browser.next();
+				load_image();
 				break;
+			
+			// Editing output
+			case K_a:
+				o.x -= xy_move_speed;
+				break;
+			case K_d:
+				o.x += xy_move_speed;
+				break;
+			case K_w:
+				o.y -= xy_move_speed;
+				break;
+			case K_s:
+				o.y += xy_move_speed;
+				break;
+			
+			case K_f:
+				o.w -= wh_move_speed;
+				break;
+			case K_h:
+				o.w += wh_move_speed;
+				break;
+			case K_t:
+				o.h += wh_move_speed;
+				break;
+			case K_g:
+				o.h -= wh_move_speed;
+				break;
+			
+			
 			default:
 				cout << "Pressed: " << key << endl;
 
 		}
-		current_path = browser.getPath();
+		
 		update_image();
 	}
 }
