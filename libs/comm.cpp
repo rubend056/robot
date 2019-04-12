43e9d3a604edced0973caccc24a7becfb56e8741
RubenD <rubendariopm14@gmail.com>
2019-04-11 20:05:24 -0400

    separating nav_serial code

--- libs/comm.cpp
+++ libs/comm.cpp
@@ -1,9 +1,6 @@
 #include <wiringSerial.h>
 #include "comm.h"
-#include "string"
-#include <unistd.h>
 #include <iostream>
-#include <vector>
 
 
 using namespace std;
@@ -72,87 +69,3 @@ Comm::~Comm()
 	cout << "The comm destructor called" << endl;
 	serialClose(serialNav);
 }
-
-
-void CommObject::serialize(ByteConstructor &bc){
-	bc.add(poly);
-	bc.add(x);
-	bc.add(y);
-	bc.add(color);
-}
-void CommObject::deserialize(ByteReceiver &br){
-	br.get(poly);
-	br.get(x);
-	br.get(y);
-	br.get(color);
-}
-void CommObject::GetBytes(std::vector<CommObject*> &objects, ByteConstructor& bc){
-	if (objects.size()==0)return;
-	bc.add<uint8_t>(objects.size());
-	for (auto o: objects)o->serialize(bc);
-}
-CommObject** CommObject::GetObjects(ByteReceiver &br, int& s){
-	s = br.get<uint8_t>();
-	CommObject **objects = new CommObject*[s];
-	for(int i=0;i<s;i++){
-		objects[i] = CommObject::Deserialize(br);
-	}
-	return objects;
-}
-void CommObject::DeleteObjects(CommObject** objects, int s){for(int i=0;i<s;i++)delete objects[i]; delete[] objects;}
-
-CommObject* CommObject::Deserialize(ByteReceiver &br){
-	CommObject o;
-	o.deserialize(br);
-	CommObject* p; if (o.poly) p = new CommPoly(o); else p = new CommCircle(o);
-	p->deserialize(br);
-	return p;
-}
-
-void CommPoly::serialize(ByteConstructor &bc){
-	CommObject::serialize(bc);
-	bc.add(sx);
-	bc.add(sy);
-}
-void CommPoly::deserialize(ByteReceiver &br){
-	br.get(sx);
-	br.get(sy);
-}
-
-void CommCircle::serialize(ByteConstructor &bc){
-	CommObject::serialize(bc);
-	bc.add(r);
-}
-void CommCircle::deserialize(ByteReceiver &br){
-	br.get(r);
-}
-
-// CommObject* CommObject::getObjects(uint8_t* d, int n, int* out_n){
-// 	int osize = (4*3+1); *out_n = n / osize;
-// 	CommObject *o = new CommObject[*out_n];
-// 	for(int i=0;i<*out_n;i++){
-// 		uint8_t* j = d + osize*i;
-// 		o[i].x = *((float*)(j));
-// 		o[i].y = *((float*)(j+4));
-// 		o[i].s = *((float*)(j+8));
-// 		o[i].square = (*(j+12) & (1 << 7)) ? 1 : 0;
-// 		o[i].color = *(j+12) & 3;
-// 	}
-// 	return o;
-// }
-// uint8_t* CommObject::getBytes(vector<CommObject> objects, int* n){
-// 	if (objects.size() == 0)return NULL;
-// 	int osize = (4*3+1); *n = objects.size() * osize;
-// 	uint8_t *d = (uint8_t*)malloc(*n); // We have our memory
-// 	for(int i=0;i<objects.size();i++){
-// 		// cout << i << " " << objects[i].x << " " << objects[i].y << " " << objects[i].s << " " << (objects[i].square ? '1' : '0') << " " << (int)objects[i].color << endl;
-// 		uint8_t* j = d + osize*i;
-// 		memcpy(j, &objects[i].x, 4);
-// 		memcpy(j+4, &objects[i].y, 4);
-// 		memcpy(j+8, &objects[i].s, 4);
-// 		uint8_t t = ((objects[i].square) << 7) | (objects[i].color);
-// 		// printBits(&t);
-// 		memcpy(j+12, &t, 1);
-// 	}
-// 	return d;
-// }
--- libs/comm.h
+++ libs/comm.h
@@ -2,8 +2,6 @@
 #define COMM_H
 
 #include "misc.h"
-#include "byte_helper.h"
-#include <vector>
 #include <stdint.h>
 
 class Comm
@@ -18,69 +16,6 @@ class Comm
         int serialNav;
 
 
-};
-
-class CommObject{
-    public:
-        
-        float x=0, y=0;
-        uint8_t color = 0;
-        bool poly = false;
-        CommObject(){}
-        CommObject(const CommObject &o):x(o.x),y(o.y),color(o.color),poly(o.poly){}
-        virtual void serialize(ByteConstructor &bc);
-        
-        virtual void print(){
-            #ifdef ARDUINO
-            Serial.print(String(x) + " " + String(x) + " " + String(color) + " " + (poly) ? "poly": "circle");
-            #else
-            cout << x << " " << y << " " << (int)color << " " << ((poly)?"poly":"circle" );
-            #endif
-        }
-        
-        virtual void deserialize(ByteReceiver &br);
-        static CommObject* Deserialize(ByteReceiver &br);
-        
-        // uint8_t type=0; // If 0 bit is on, then square else sphere, 1-7th bits are used for colors, since only 4 colors only 6-7 bits will be used\
-        meaning blue, green, red, yellow, in that order.
-        static void GetBytes(std::vector<CommObject*> &objects, ByteConstructor& bc);
-    	static CommObject** GetObjects(ByteReceiver &br, int& s);
-        static void DeleteObjects(CommObject** objects, int s);
-};
-
-
-class CommPoly :public CommObject{
-public:
-    float sx=0, sy=0;
-    CommPoly(){poly=true;}
-    CommPoly(const CommObject &o):CommObject::CommObject(o){}
-    void serialize(ByteConstructor &bc);
-    void deserialize(ByteReceiver &br);
-    void print(){
-        CommObject::print();
-        #ifdef ARDUINO
-        Serial.println(" " + String(sx) + " " + String(sy));
-        #else
-        cout << " " << sx << " " << sy << endl;
-        #endif
-    }
-};
-
-class CommCircle:public CommObject{
-public:
-    float r=0;
-    CommCircle(){poly=false;}
-    CommCircle(const CommObject &o):CommObject::CommObject(o){}
-    void serialize(ByteConstructor &bc);
-    void deserialize(ByteReceiver &br);
-    void print(){
-        CommObject::print();
-        #ifdef ARDUINO
-        Serial.println(" " + String(r));
-        #else
-        cout << " " << r << endl;
-        #endif
-    }
 };
 
 #endif
